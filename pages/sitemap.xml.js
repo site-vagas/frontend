f8b99fcd909062d1cf9c89bed6e9f0029851c04c
Warley Carvalho <warleyc2009@gmail.com>
2021-07-11 18:12:33 -0300

    Sitemap and Job Apply Window

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -9,11 +9,16 @@ export async function getServerSideProps({ res }){
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://www.tickun.com.br</loc>
+            <lastmod>2021-07-01</lastmod>
+        </url>
+
         ${
             jobs.map( (job) => (
                 `<url>
-                    <loc>https://tickun.com.br/vaga/${job.code}/${job.slug}</loc>
-                    <lastmod>2018-06-04</lastmod>
+                    <loc>https://www.tickun.com.br/vaga/${job.code}/${job.slug}</loc>
+                    <lastmod>${ job.updatedAt ? job.updatedAt : job.createdAt }</lastmod>
                 </url>`
             ))
         }
@@ -21,8 +26,8 @@ export async function getServerSideProps({ res }){
         ${
             organizations.map( (organization) => (
                 `<url>
-                    <loc>https://tickun.com.br/empresa/${organization.code}/${organization.slug}</loc>
-                    <lastmod>2018-06-04</lastmod>
+                    <loc>https://www.tickun.com.br/empresa/${organization.code}/${organization.slug}</loc>
+                    <lastmod>${ organization.updatedAt ? organization.updatedAt : organization.createdAt }</lastmod>
                 </url>`
             ))
         }
