38e0a98306df56310c379ce22603896823d6a5ef
Warley Carvalho <warleyc2009@gmail.com>
2021-10-04 00:12:25 -0300

    Add Google Adsense Script

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ class MyDocument extends Document {
             <link rel="icon" sizes="192x192" href="/favicon.png" />
 
             {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+            <script data-ad-client={`${process.env.GOOGLE_ADSENSE_KEY}`} async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_KEY}`}></script>
             <script dangerouslySetInnerHTML={{
               __html: `
